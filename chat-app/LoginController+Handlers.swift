e5c2db62655bff6b63e26b220c5917db71b16cc0
martinnord <martin.nordstrom99@gmail.com>
2017-05-09 08:27:45 +0200

    User can now add profile pic

--- chat-app/LoginController+Handlers.swift
+++ chat-app/LoginController+Handlers.swift
@@ -14,15 +14,36 @@ extension LoginController: UIImagePickerControllerDelegate, UINavigationControll
         let picker = UIImagePickerController()
         
         picker.delegate = self
+        picker.allowsEditing = true
         
         present(picker, animated: true, completion: nil)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        print(info)
+        
+        var selectedImageFromPicker: UIImage?
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage{
+        
+            selectedImageFromPicker = editedImage
+            
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+        
+            selectedImageFromPicker = originalImage
+            
+        }
+        
+        if let selectedImage = selectedImageFromPicker {
+            profileImageView.image = selectedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+
+        
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
         print("canceled")
     }
     
