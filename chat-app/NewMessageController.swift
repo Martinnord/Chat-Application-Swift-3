592b25dd0290065c0f03cd3dc5d80fdf98417ec9
martinnord <martin.nordstrom99@gmail.com>
2017-05-20 22:45:44 +0200

    Styled the profile image

--- chat-app/NewMessageController.swift
+++ chat-app/NewMessageController.swift
@@ -18,7 +18,7 @@ class NewMessageController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tableView.backgroundColor = UIColor().HexToColor(hexString: "#2D2F56", alpha: 1.0)
+        //self.tableView.backgroundColor = UIColor().HexToColor(hexString: "#2D2F56", alpha: 1.0)
 
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
         
@@ -62,7 +62,7 @@ class NewMessageController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! UserCell
         
         // Styling the font
         cell.textLabel?.textColor = UIColor.white
@@ -70,10 +70,6 @@ class NewMessageController: UITableViewController {
         
         // No highlighed tableviewcell
         cell.selectionStyle = .none
-        
-        // Styling the profile image icons
-        cell.imageView?.contentMode = .scaleAspectFit
-
 
         let user = users[indexPath.row]
         cell.textLabel?.text = user.name
@@ -92,19 +88,49 @@ class NewMessageController: UITableViewController {
                 
                 // Success!
                 DispatchQueue.main.async() {
-                    cell.imageView?.image = UIImage(data: data!)
-
+                    cell.profileImageView.image = UIImage(data: data!)
                 }
+            
             }).resume()
         }
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 56
+    }
 }
 
 class UserCell: UITableViewCell {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel?.frame = CGRect(x: 56, y: textLabel!.frame.origin.y, width: textLabel!.frame.width, height: textLabel!.frame.height)
+        
+        detailTextLabel?.frame = CGRect(x: 56, y: detailTextLabel!.frame.origin.y, width: detailTextLabel!.frame.width, height: detailTextLabel!.frame.height)
+
+    }
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        
+        // need x,y,width and height anchors
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
